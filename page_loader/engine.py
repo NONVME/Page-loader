53063f8a0e27f2e4c8565946661e902278132ba5
Chashin Efim <69835249+NONVME@users.noreply.github.com>
2020-12-30 15:52:04 +0000

    Add: basic request

--- page_loader/engine.py
+++ page_loader/engine.py
@@ -1,12 +1,26 @@
 import re
 import os
 
+import requests
+
 
 def download(url, output):
     file_name = generate_name(url, output)
+    save_page(get_page(url).text, file_name)
     return file_name
 
 
 def generate_name(url, output):
     url = re.sub(r'\W', '-', re.sub(r'^([a-zA-Z]+):\/\/', '', url))
     return os.path.join(output, url + '.html')
+
+
+def get_page(url):
+    headers = {'user-agent': 'my-app/0.0.1'}
+    r = requests.get(url, headers=headers, timeout=1)
+    return r
+
+
+def save_page(page, path):
+    with open(path, 'w', encoding='utf-8') as file:
+        file.write(page)
