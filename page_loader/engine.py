0c2efdcb2a4805da2489e844145e59331ea385e7
Chashin Efim <69835249+NONVME@users.noreply.github.com>
2021-01-23 05:05:11 +0000

    chore: naming fix

--- page_loader/engine.py
+++ page_loader/engine.py
@@ -7,18 +7,17 @@ from page_loader.url_formatter import get_domain, url_normalize
 
 def download(url: str, dir_path: str) -> str:
     url = url_normalize(url)
-    page_dirname = to_dirname(dir_path, url, '.html')
-    assets_dirname = to_dirname(dir_path, url, '_file')
-
+    full_dirname = to_dirname(dir_path, url, '.html')
     page = get_data(url).text
     modified_page, original_links = get_modified_page(url, page, dir_path)
-    write(modified_page, page_dirname)
+    write(modified_page, full_dirname)
+    assets_dirname = to_dirname(dir_path, url, '_file')
 
     for link in original_links:
         domain_link = url_normalize(get_domain(url) + link)
-        assets_full_dirname = to_dirname(assets_dirname, url,
-                                         link, is_link=True)
-        resources = get_data(domain_link).content
-        write(resources, assets_full_dirname)
+        assets_full_dirname = to_dirname(assets_dirname, url, link,
+                                         is_link=True)
+        page = get_data(domain_link).content
+        write(page, assets_full_dirname)
 
-    return page_dirname
+    return full_dirname
